fec59ad856342b4a3a3ea9b399ed6932d9a87bbf
RealDebato <baraldennis99@googlemail.com>
2023-10-24 13:07:49 +0200

    Changes to be committed: 	modified:   sheet1_problem1.py

--- sheet1_problem1.py
+++ sheet1_problem1.py
@@ -17,30 +17,18 @@ import math
 #beta = 1
 
 
-""" def f(var, alpha, beta):
+def f(var, alpha, beta):
     fy = math.exp(-alpha*var) + pow(var, beta)
     return fy
 
 
+print(f(1, 1, 1))
 
-def convFun(f, a, b, eps):               # f: convexe Funktion, a: untere Intervallgrenze, b: obere Intervallgrenze, eps: gewünschte max. Genauigkeit
-    
+
+#print(convFun(f, 0, 1, 0.001))
+
+def convFun(a, b, eps, alpha, beta, f):     # Intervall [a,b], Toleranz
     e = b - a
-    while e > eps:
-        if f(a, 1, 1) > f(b, 1, 1):
-            a = a - eps
-        else:
-            b = b - eps
-    return fu(a + b/2)
-
-print(convFun(f))
-
-
-#print(convFun(f, 0, 1, 0.001)) """
-
-def convFun(a, b, eps, alpha, beta):     # Intervall [a,b], Toleranz
-    e = b - a
-    var = 1
     #math.exp(-alpha*var) + pow(var, beta)
     while e > eps:
         # Grenzen der Trisektion bestimmen
@@ -50,13 +38,14 @@ def convFun(a, b, eps, alpha, beta):     # Intervall [a,b], Toleranz
         x2 = b - s
         x3 = b
 
-        Seg1 = math.exp(-alpha*x1) + pow(x1, beta) - math.exp(-alpha*x0) + pow(x0, beta)
-        Seg2 = math.exp(-alpha*x2) + pow(x2, beta) - math.exp(-alpha*x1) + pow(x1, beta)
-        Seg3 = math.exp(-alpha*x3) + pow(x3, beta) - math.exp(-alpha*x2) + pow(x2, beta)
+        Seg1 = ((math.exp(-alpha*x1) + pow(x1, beta) - math.exp(-alpha*x0) + pow(x0, beta))/2) + math.exp(-alpha*x0) + pow(x0, beta)
+        Seg2 = ((math.exp(-alpha*x2) + pow(x2, beta) - math.exp(-alpha*x1) + pow(x1, beta))/2) + math.exp(-alpha*x1) + pow(x1, beta)
+        Seg3 = ((math.exp(-alpha*x3) + pow(x3, beta) - math.exp(-alpha*x2) + pow(x2, beta))/2) + math.exp(-alpha*x2) + pow(x2, beta)
 
         print('Seg1 =', Seg1)
         print('Seg2 =', Seg2)
         print('Seg3 =', Seg3)
+        print('X0-3 =', x0, x1, x2, x3)
 
         if Seg1 > Seg2 < Seg3:
             a = x1
@@ -75,7 +64,7 @@ def convFun(a, b, eps, alpha, beta):     # Intervall [a,b], Toleranz
     return a + e/2
 
 
-print('Min =', convFun(0, 1, 0.01, 2, 1))
+print('Min =', convFun(0, 1, 0.01, 2, 1, f(1,1,1)))
 
 
 
