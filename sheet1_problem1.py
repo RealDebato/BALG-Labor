eed3550ac72e0c1c767a202a3d1e8764f43ecba1
RealDebato <baraldennis99@googlemail.com>
2023-10-24 21:15:12 +0200

    Changes to be committed: 	modified:   sheet1_problem1.py
    Funktion getestet und erfolgreich.
    Befindet sich das Minimum auf einer Intervalgrenze kann das
    Minimum nicht ermittelt werden.
    Folgende Ideen
    - Startgrenzen des intervall in Merkervariablen speichern
    - Sollten alle Steigungen positiv sein [1, 1, 1] entspricht muss das
      Minimum vor dem Interval liegen
    - Sollten alle Steigungen negativ sein [-1, -1, -1] entspricht muss das
      Minimum hinter dem Interval liegen
    
    Anstatt kein Minimum auszugenen sollten oben genannte Informationen
    ausgegeben werden

--- sheet1_problem1.py
+++ sheet1_problem1.py
@@ -4,45 +4,18 @@
 # function
 
 import numpy as np
-import scipy as sp
 import math
 
-#print('Alpha =')
-#alpha = input()
-
-#print('Beta =')
-#beta = input()
-
 alpha = 5
-beta = 1
+beta = 5
 
 
-def f(var, alpha, beta):
-    fy = math.exp(-alpha*var) + pow(var, beta)
+def f(var, a, b):
+    fy = math.exp(-a*var) + pow(var, b)
     return fy
 
-fu = f(1, alpha, beta)
-
-print(fu)
-
-
-#print(convFun(f, 0, 1, 0.001))
-
 def convFun(a, b, eps):     # Intervall [a,b], Toleranz eps
-    
-    
-    #s_start = (b - a)/3
-    #x0_start = a
-    #x1_start = a + s_start
-    #x2_start = b - s_start
-    #x3_start = b
-
-    # Abbruchbedingung: Erst wenn in allen drei Segmenten die Differenz der benachbarten Stüzpunkten in y geringer wie die Toleranz ist, sind wir nah genug am Minimum.  
-    #e1 = abs(f(x0, 1, 1) - f(x1, 1, 1))
-    #e2 = abs(f(x1, 1, 1) - f(x2, 1, 1))
-    #e3 = abs(f(x2, 1, 1) - f(x3, 1, 1))
-
-
+       
     while True:
         # Grenzen der Trisektion bestimmen
         s = (b - a)/3
@@ -50,48 +23,52 @@ def convFun(a, b, eps):     # Intervall [a,b], Toleranz eps
         x1 = a + s
         x2 = b - s
         x3 = b
-
-        print('X[]', x0, x1, x2, x3)
+        #print('X[]', x0, x1, x2, x3)
 
         #Steigungen zwischen den Segmentgrenzen berechen
         m_seg1 = (f(x1, alpha, beta) - f(x0, alpha, beta))/(x1 - x0)
         m_seg2 = (f(x2, alpha, beta) - f(x1, alpha, beta))/(x2 - x1)
         m_seg3 = (f(x3, alpha, beta) - f(x2, alpha, beta))/(x3 - x2)
 
-        #print('m Seg', m_seg1, m_seg2, m_seg3),
+        # Vorzeichen der Segmentsteigungen auslesen
         M_Seg = [m_seg1, m_seg2, m_seg3]
-        print('M Seg =', M_Seg)
+        #print('M Seg =', M_Seg)
         M_seg_direction = np.sign(M_Seg)
-        print('M Seg Direction =', M_seg_direction)
+        #print('M Seg Direction =', M_seg_direction)
 
         # Entsprechend der Steigungen eines der äußeren Segmente löschen
-        if np.array_equal(M_seg_direction, [-1,1,1]):
+        if np.array_equal(M_seg_direction, [-1,1,1]):           # Tiefpunkt liegt links
             a = x0
             b = x2
-        elif np.array_equal(M_seg_direction, [-1,-1,1]):
+        elif np.array_equal(M_seg_direction, [-1,-1,1]):        # Tiefpunkt liegt rechts
             a = x1
             b = x3
-        else:
+        else:                                                   # Funktion ist im Interval streng monoton 
             print('Im Interval wurde keine Minimum gefunden')
-            break
+            return None
+
 
         e1 = abs(f(x0, 1, 1) - f(x1, 1, 1))
         e2 = abs(f(x1, 1, 1) - f(x2, 1, 1))
         e3 = abs(f(x2, 1, 1) - f(x3, 1, 1))
 
-        e = (b - a)/2
+        e = (b - a)/2       
 
         if (e1 < eps) & (e2 < eps) & (e3 < eps):
             break
-        
-        
-        
+
     fx_min = a + e/2
-    fy_min = f(a + e/2, 1, 1)
-    return fx_min, fy_min
+    fy_min = f(a + e/2, alpha, beta)
+    return fx_min, fy_min,
 
 
-print('Min(x, y) =', convFun(0, 1, 0.01))
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Testing
+
+print('Min(x, y) =', convFun(0, 1, 0.001))
+
+
 
 
 
