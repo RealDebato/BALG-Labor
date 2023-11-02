b41af94d371baac1896a65e3ce900c0b99962042
RealDebato <baraldennis99@googlemail.com>
2023-11-02 15:02:54 +0100

    Changes to be committed: 	new file:   .vscode/launch.json 	modified:   _Smiley.png 	modified:   sheet1_problem3.py 	modified:   test.py 	deleted:    test_2.py

--- sheet1_problem3.py
+++ sheet1_problem3.py
@@ -1,119 +1,87 @@
-# Sheet 1, Problem 3
-# Compute the local entropy for a circle of radius r. Filter welcher die Entropie eines Bildes bestimmt
-
-
 import numpy as np
 import math
 import scipy as sp
 import cv2
 import matplotlib.pyplot as plt
 
-# Bild erstellen von welchem die lokale Entropie ermittelt wird
 
-#img1 = np.random.randint(255, size=(100, 100))
-Eye = np.eye(500) * 255
-Kermit = cv2.imread('_Kermit.png', 0)
-#Smiley = cv2.imread('_Smiley.png', 0)
-#cv2.imshow('Orginal Eye', Eye)
+Kermit = cv2.imread('_Smiley.png', 0)
 cv2.imshow('Orginal Kermit', Kermit)
-#cv2.imshow('Orginal Smiley', Smiley)
+#Kermit = np.ones((100, 100)).astype('uint8')
+
+'''Kermit = np.array([[0, 0, 155, 0, 0],
+                  [0, 155, 200, 155, 0],
+                  [155, 200, 255, 200, 155],
+                  [0, 155, 200, 155, 0],
+                  [0, 0, 155, 0, 0]])'''
+
+
 
 def hist_n(img):
     histogram = np.zeros(256)
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
-            GV = img[i, j]
-            histogram[GV] = histogram[GV] + 1
-    histogram_normal = histogram/histogram.sum()
-    return histogram_normal
+    
+    if img.ndim == 2:
+       for i in range(0, img.shape[0]):
+              for j in range(0, img.shape[1]):
+                     GV = img[i, j]
+                     GV = int(GV)                     
+                     histogram[GV] = histogram[GV] + 1                     
+       histogram_normal = histogram/histogram.sum()
+       return histogram_normal
+    elif img.ndim == 1:
+       for i in range(0, img.shape[0]):
+              GV = img[i]
+              GV = int(GV)                       # Warum hier nötig aber nicht bei if img.ndim == 2 ???
+              histogram[GV] = histogram[GV] + 1              
+       histogram_normal = histogram/histogram.sum()
+       return histogram_normal
 
 Histn_Kermit = hist_n(Kermit)
-max_px = max(Histn_Kermit)
+max_hist = max(Histn_Kermit)
 
-def Entropy(p):
-    if p > 0:
-        E = -(p * np.log2(p)).sum()
-    return E
+def Entropy(p):                                         # Argument sind die Wahrscheinlichkeiten als Vektor 
+       E = -(p[p > 0] * np.log2(p[p > 0])).sum()
+       return E
 
+print(Entropy(Histn_Kermit))
 
+#filter_kern_q = np.array([1, 1, 1],
+#                         [1, 1, 1],
+#                         [1, 1, 1])
 
 def entropy_filter_slow(img, r):                 # img[col, row]
-    core = 0
-    # Randberech erweitern
-    img = np.pad(array=img, pad_width=r, mode='constant', constant_values=0)
-    #cv2.imshow('Mit Rand', img)
-        
+       p = []
+       core = 0
+       # Randberech erweitern
+       img = np.pad(array=img, pad_width=r, mode='constant', constant_values=0)
+       #print('Img Pad=', img)
+    
+       for i in range(r, img.shape[0] - r):
+              #print('i =', i)
+              for j in range(r, img.shape[1] - r):
+                     #print('j =', j)
+                     for k in range(-r, r + 1):
+                            #print('k =', k)
+                            for n in range(-r, r + 1):
+                                   #print('n =', n)
+                                                                  # 1. Filterposition pro Col muss komplett berechnet werden
+                                   gw = img[i + k, j + n]
+                                   #print('gw(i + k, j + n) =', gw)
+                                   gw = int(gw)
+                                   p = np.append(p, gw)
+                                   #print('p =', p)
 
+                                   
+                     histn_p = hist_n(p)
+                     core = core + Entropy(histn_p)
+                     img[i, j] = core
 
-    for i in range(r, img.shape[0] - r):
-        for j in range(r, img.shape[1] - r):
-            for k in range(-r, r):
-                for l in range(-r, r):
-                    p = img[i + k, j + l]
-                    core = core + Entropy(p)
-            img[i, j] = core
+       return img
 
-    return img
+Kermit_Entropy_filtered = entropy_filter_slow(Kermit, 1)
+print('Filtered Kermit=', Kermit_Entropy_filtered)
 
-#entropy_eye = entropy_filter_slow(Eye, 1)
-entropy_kermit = entropy_filter_slow(Kermit, 1)
-#entropy_smiley = entropy_filter_slow(Smiley, 1)
-#cv2.imshow('Entropy Eye', entropy_eye)                    
-cv2.imshow('Entropy Kermit', entropy_kermit)
-#cv2.imshow('Entropy Smiley', entropy_smiley)
-
-Hist_Kermit_entropy = hist(entropy_kermit)
-
-plt.style.use('_mpl-gallery')
-
-fig, ax = plt.subplots()
-
-ax.stairs(Hist_Kermit, linewidth=2.5)
-
-ax.set(xlim=(0, 256), xticks=np.arange(1, 256),
-       ylim=(0, max_px), yticks=np.arange(0, max_px, 0.1))
-
-plt.show()
+cv2.imshow('Entropie-Bild', Kermit_Entropy_filtered)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows() 
-
-'''def entropy_filter(img, r):
-    i = 0
-    j = 0
-    u = 0
-    entropy_core = 0
-    entropy_col_1 = 0
-    entropy_col_2 = 0
-    entropy_col_3 = 0
-    entropy_col_4 = 0
-    for u in range(0, 2 * r - 1):               # Bedingung alle Filterkerne sind symmetrisch, d. h. r ist ungerade aus N
-        for v in range(0, 2 * r - 1):           # Werden gerade r zugelassen muss muss zuvor abgefragt werden ob r gerade/ungerade
-            entropy_core = entropy_core + Entropy(img[u, v] / 255)
-            if u == 0:
-                entropy_col_1 = entropy_col_1 + Entropy(img[u, v] / 255)
-            elif u == 1:
-                entropy_col_2 = entropy_col_2 + Entropy(img[u, v] / 255)
-            elif u == 2:
-                entropy_col_3 = entropy_col_3 + Entropy(img[u, v] / 255)
-
-            
-        
-    for i in range(1 + r, img.shape[0] - r):            # Img[col, row]
-        for j in range(1 + r , img.shape[1] - r):       
-            for k in range(-r, r):
-                entropy_col_1 = entropy_col_1 + Entropy(img[i + k, j - r - 1])
-                entropy_col_4 = entropy_col_4 + Entropy(img[i + k, j + r])
-
-            entropy_core = entropy_core - entropy_col_1 + entropy_col_4
-            img[i, j] = entropy_core
-
-    return img'''
-    
-   
-
-
-                
-
-
-
--- test.py
+++ test.py
@@ -7,33 +7,13 @@ import matplotlib.pyplot as plt
 
 
 
-Kermit = cv2.imread('_Kermit.png', 0)
+Kermit = np.array([[0, 0, 155, 0, 0],
+                  [0, 155, 200, 155, 0],
+                  [155, 200, 255, 200, 155],
+                  [0, 155, 200, 155, 0],
+                  [0, 0, 155, 0, 0]])
 
-def hist(img):
-    histogram = np.zeros(256)
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
-            GV = img[i, j]
-            histogram[GV] = histogram[GV] + 1
-    return histogram
-
-pixelanzahl = Kermit.shape[0] * Kermit.shape[1]
-print(pixelanzahl)
-Hist_Kermit = hist(Kermit)
-Hist_Kermit = Hist_Kermit / pixelanzahl
-max_px = max(Hist_Kermit)
-print(Hist_Kermit)
-
-plt.style.use('_mpl-gallery')
-
-fig, ax = plt.subplots()
-
-ax.stairs(Hist_Kermit, linewidth=2.5)
-
-ax.set(xlim=(0, 256), xticks=np.arange(1, 256),
-       ylim=(0, max_px), yticks=np.arange(0, max_px, 0.1))
-
-plt.show()
-
+print(Kermit.ndim)
+print(Kermit.shape)
 
 
