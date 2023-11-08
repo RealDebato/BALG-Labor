2766ed7fccb44005cd3df92bfc4f573b13b118c8
RealDebato <baraldennis99@googlemail.com>
2023-11-08 20:14:12 +0100

    Changes to be committed: 	modified:   sheet2/sheet2_problem1.py

--- sheet2/sheet2_problem1.py
+++ sheet2/sheet2_problem1.py
@@ -1,24 +1,69 @@
 import numpy as np
 import math
-import scipy as sp
+import scipy
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('sheet2\Test_Images\city.jpg', 0)
+img = cv2.imread('sheet2\Test_Images\Retina1.jpg')
+
 cv2.imshow('Orginal', img)
 
-img_linear =
+c = 0.2
+r = 5
 
-img_morph =
+c_smooth = 0.2
 
-img_tophat =
+#lin_laplace = img*0
+#morph_laplace = img*0
+kernel = np.ones((r, r), np.uint8)
+
+# Ohne Smoothing
+#--------------------------------------------------------------------------------------------------
+# Linear Laplacian
+lin_laplace = cv2.Laplacian(img, ddepth=0, ksize=r)
+S_lin = img - (c * lin_laplace).astype(np.uint8)
+
+# Morphological Laplacian --->  0.5 * (Dilatation + Erosion) - img
+morph_laplace = 0.5 * (cv2.dilate(img, kernel, iterations=1) + cv2.erode(img, kernel, iterations=1))
+S_morph = img - (c * morph_laplace).astype(np.uint8)
+
+# Tophat
+WTH = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel)
+BTH = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
+S_tophat = img - (c * WTH).astype(np.uint8) + (c * BTH).astype(np.uint8)
 
 
+cv2.imshow('Lin Laplace', S_lin)
+cv2.imshow('Morph Laplace', S_morph)
+cv2.imshow('Tophat', S_tophat)
 
 
+# Mit Smoothing
+#--------------------------------------------------------------------------------------------------
+smooth_gaussian = cv2.GaussianBlur(img, (r, r), 0)
+cv2.imshow('Gaussian', smooth_gaussian)
 
-#cv2.imshow('Entropie-Bild', Kermit_Entropy_filtered)
-#print('Laufzeit', Kermit_Entropy_filtered)
+# Linear Laplacian
+lin_laplace_S = cv2.Laplacian(smooth_gaussian, ddepth=0, ksize=r)
+S_lin_smooth = smooth_gaussian - (c_smooth * lin_laplace_S).astype(np.uint8)
+
+# Morphological Laplacian --->  0.5 * (Dilatation + Erosion) - img
+morph_laplace_S = 0.5 * (cv2.dilate(smooth_gaussian, kernel, iterations=1) + cv2.erode(smooth_gaussian, kernel, iterations=1))
+S_morph_smooth = img - (c_smooth * morph_laplace_S).astype(np.uint8)
+
+# Tophat
+WTH_S = cv2.morphologyEx(smooth_gaussian, cv2.MORPH_TOPHAT, kernel)
+BTH_S = cv2.morphologyEx(smooth_gaussian, cv2.MORPH_BLACKHAT, kernel)
+S_tophat_smooth = smooth_gaussian - (c_smooth * WTH).astype(np.uint8) + (c_smooth * BTH).astype(np.uint8)
+
+cv2.imshow('Lin Laplace smooth', S_lin_smooth)
+cv2.imshow('Morph Laplace smooth', S_morph_smooth)
+cv2.imshow('Tophat smooth', S_tophat_smooth)
+
+
+cv2.imshow('Diff', img-S_tophat)
+
+#print('Laufzeit', laplace_img)
 #print('Laufzeit =', t1-t0 )
 print('Ende')
 cv2.waitKey(0)
