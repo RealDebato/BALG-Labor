fb22f5a6559ea7f14c11ec8316ee92b7c8d63e82
RealDebato <baraldennis99@googlemail.com>
2023-11-14 13:56:20 +0100

    Changes to be committed: 	modified:   sheet2/sheet2_problem2.py

--- sheet2/sheet2_problem2.py
+++ sheet2/sheet2_problem2.py
@@ -4,12 +4,16 @@ import scipy
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('sheet2\Test_Images\Retina1.jpg', 0)
+# Reconstruction by erosion
+
+img = cv2.imread('sheet2\Test_Images\particle1.jpg', 0)
+img = np.abs(255 - img)
+plt.imshow(img)
 
 cv2.imshow('Orginal', img)
 
 r = 1
-kernel = np.ones((r, r), np.uint8)
+kernel = np.ones((3, 3), np.uint8)
 
 def gray_erosion(img, r):
     img = np.array(img)
@@ -29,10 +33,53 @@ def gray_erosion(img, r):
             print(filtered_img[y, x])
     return filtered_img
 
-Erosion = gray_erosion(img, r).astype(np.uint8)
+def ero_recon(img, kernel, eps):
+    
+    i = 0
+    
+    erosion_img = cv2.erode(img, kernel, iterations=1)
+    while True:
+        if np.abs(np.subtract(img, erosion_img)).all() > eps:
+            erosion_img = cv2.erode(img, kernel, iterations=1)
+            i = i + 1
+        elif i > 100:
+            print('Laufzeit zu lang')
+            break
+        else:
+            return erosion_img
 
-#Erosion_plot = plt.imshow(Erosion)
-cv2.imshow('Erosion', Erosion)
+#---------------------------------------------------------------------------------------------------
+# Startpunkte mit Erosion bestimmen
+
+start = cv2.erode(img, kernel, iterations=15)
+
+cv2.imshow('Start', start)
+
+
+# Reconstruction by dilation
+Recon = 255 * (np.logical_and(img, cv2.dilate(start, kernel, iterations=1)).astype(np.uint8))
+Recon_old = np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8)
+i = 0
+
+while Recon.all() == Recon_old.all():
+    Recon = 255 * (np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8))
+    Recon_old = np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8)
+    i = i + 1
+    if i == 100:
+        break
+    
+
+cv2.imshow('Recon', Recon)
+
+plt.imshow(img)
+
+
+
+#cv2.imshow('Diff', img - Recon)
+
+
+
+#cv2.imshow('Erosion', Erosion)
 
 
 print('Ende')
