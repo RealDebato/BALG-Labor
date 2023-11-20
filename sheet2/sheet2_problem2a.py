d1cfd7c2e0cdb09474a78690a72f351e7742ce9c
RealDebato <baraldennis99@googlemail.com>
2023-11-20 19:41:33 +0100

    Changes to be committed: 	renamed:    sheet2/sheet2_problem2.py -> sheet2/sheet2_problem2a.py

--- sheet2/sheet2_problem2a.py
+++ sheet2/sheet2_problem2a.py
@@ -23,25 +23,25 @@ kernel = np.ones((3, 3), np.uint8)
 
 def recon_by_dilation_binary(img, kernel,  start_iter):
 
-    start = cv2.erode(img, kernel, iterations=start_iter)
+    start = cv2.erode(img, kernel, iterations=start_iter)                           # Startmarker erstellen (Seed), hier mit erosion bis alle kleinen Elemente weg sind
     #cv2.imshow('Start1', start)
 
     # Reconstruction by dilation
     t0_selfmade = time.time()
-    Recon = 255 * (np.logical_and(img, cv2.dilate(start, kernel, iterations=1)).astype(np.uint8))
-    Recon_old = 255* (np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8))
-    i = 0
+    Recon = 255 * (np.logical_and(img, cv2.dilate(start, kernel, iterations=1)).astype(np.uint8))       # Erste, ini Reconstruktion. Seed/Start wurde mit Erosion erstellt als wird Recon mit dilation
+    Recon_old = 255* (np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8))    # Zweiter Recon wird initiert. --> Bild wurde zweimal dilatiert.
+    # Ini könnte auch ohne dilate z. B.  als np.zero_like(img) ---> Gefahr dass zweimal dilatieren schon zu viel sein könnte
 
-    while np.array_equal(Recon, Recon_old) == False:
-        Recon = 255 * (np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8))
-        Recon_old = 255 * (np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8))
-
+    while np.array_equal(Recon, Recon_old) == False:                                                            # Hier beginnt eigentliche Recon
+        Recon = 255 * (np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8))           # Recon ist 1-mal dilate, Recon_old ist dann 2-mal dilate
+        Recon_old = 255 * (np.logical_and(img, cv2.dilate(Recon, kernel, iterations=1)).astype(np.uint8))       # und hiervon nur die Pixel welche sich in beiden Recon und img befinden (logical and)
+    # Erst wenn bei der zweiten Dilation keine neuen Pixel hinzukommen, d. h. wenn recon == recon_old, endet die Schleife und das Bild ist rekonstruiert
     
     t1_selfmade = time.time()
     return Recon, t1_selfmade - t0_selfmade
 
-def recon_by_dilation_grey(img, kernel, start_iter):
-    img = img.astype(np.double)
+def recon_by_dilation_grey(img, kernel, start_iter):                    # Bei grey images identisch wie binär, nur der Abgleich zum Orginalbild erfolgt nicht durch ein log and
+    img = img.astype(np.double)                                         # sondern durch das Minimum von img und dilate
     start = cv2.erode(img, kernel, iterations=start_iter)
     #cv2.imshow('Start2', start)
 
