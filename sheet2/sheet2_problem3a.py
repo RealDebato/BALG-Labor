392a755354d155d79f8fa49990d0e9fe5f47fc2a
RealDebato <baraldennis99@googlemail.com>
2023-11-20 20:23:59 +0100

    Changes to be committed: 	new file:   sheet2/Test_Images/same_circles.png 	modified:   sheet2/sheet2_problem3a.py

--- sheet2/sheet2_problem3a.py
+++ sheet2/sheet2_problem3a.py
@@ -29,26 +29,27 @@ def plot_image_to_3D(plot_3d):
 #---------------------------------------------------------------------------------------------------
 # images
 
-img = cv2.imread(R'sheet2\Test_Images\many_circles.png', 0)
+img = cv2.imread(R'sheet2\Test_Images\same_circles.png', 0)
 _, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
 #---------------------------------------------------------------------------------------------------
 # main
 def watershed_full(img, switch):
-    dmap = cv2.distanceTransform(img, cv2.DIST_L2, 3)
-    local_max = ski.feature.peak_local_max(dmap, min_distance=10)
+    dmap = cv2.distanceTransform(img, cv2.DIST_L2, 3)                   # Distance map wird erstellt
+    local_max = ski.feature.peak_local_max(dmap, min_distance=10)       # Locale Maximas aus Distance map
 
     seed = np.zeros_like(img)
-    seed[tuple(local_max.T)] = 255
-    flood_area = cv2.subtract(img, seed)
+    seed[tuple(local_max.T)] = 255                                      # Locale Maximas in Seed-Bild einfügen
+    flood_area = cv2.subtract(img, seed)                                # Flood area entspricht der Zone zwischen der Maximas und dem schwarzen (=0) Hintergrund
+                                                                        # hier wird geflutet
 
-    _, labels = cv2.connectedComponents(seed)
-    labels = labels + 1
+    _, labels = cv2.connectedComponents(seed)                           # Alles Maximas nummerieren
+    labels = labels + 1                                                 # Labels werden um +1 erhöht, sodass die Label-Nummer 0 frei wird
 
-    labels[flood_area==255] = 0
+    labels[flood_area==255] = 0                                         # Alles was jetzt im Labelbild 0 ist wird geflutet
 
     labels = np.int32(labels)
-    img = cv2.merge((img, np.zeros_like(img), np.zeros_like(img)))
+    img = cv2.merge((img, np.zeros_like(img), np.zeros_like(img)))      # OpenCV watershed ist doof und braucht zwingend ein 3-channel Bild
 
     watershed = cv2.watershed(img, labels)
     seg_img = (watershed/watershed.max()) * 255
