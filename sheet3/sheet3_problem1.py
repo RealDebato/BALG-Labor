f8e65e8b23f386ec4da74cd636398da0933c5ef0
RealDebato <baraldennis99@googlemail.com>
2023-11-22 19:43:09 +0100

    Changes to be committed: 	modified:   sheet3/sheet3_problem1.py

--- sheet3/sheet3_problem1.py
+++ sheet3/sheet3_problem1.py
@@ -46,34 +46,32 @@ label_decoder = unpickle(R'sheet3\CIFAR\batches.meta.txt')             # Index =
 data_batch_1 = unpickle(R'sheet3\CIFAR\data_batch_1.bin')
 
 pixel_data_batch_1 = np.asarray(data_batch_1[b'data'])
-pixel_data_batch_1 = np.split(pixel_data_batch_1, 10000)    # hier steht jeweils ein Array mit allen Pixelwerten aus einem Bild (1. Array --> 1. Bild, 2. Array --> 2. Bild, usw.)
+pixel_data_batch_1_single_img = np.split(pixel_data_batch_1, 10000)    # hier steht jeweils ein Array mit allen Pixelwerten aus einem Bild (1. Array --> 1. Bild, 2. Array --> 2. Bild, usw.)
+#print(pixel_data_batch_1_single_img[0])
 labels_data_batch_1 = np.asarray(data_batch_1[b'labels'])
-
-
-print(pixel_data_batch_1.size)
 
 Bild1 = image_from_cifar10(pixel_data_batch_1[nr_bild,:])
 
 
 # Wie sehen die Entscheidungsräume aus??
-# Features sollen die Pixel sein, d. h. ich machs einfach und nehm den Mittelwert aller Pixel im gleichen Farbraum
+# Features sollen die Pixel sein, d. h. ich nehm den Mittelwert aller Pixel
 
 feature_space_x_axis = np.arange(0, 1023)   #red
 feature_space_y_axis = np.arange(0, 1023)   #green
 feature_space_z_axis = np.arange(0, 1023)   #blue
 
-feature_space = np.full((1024, 1024, 1024), fill_value=-1)
-
-X, Y, Z = np.meshgrid(feature_space_x_axis, feature_space_y_axis, feature_space_z_axis)
-
+feature_space = np.full((256, 256, 256, 10), fill_value=10, dtype=np.uint16)
 
 
 # feature space mit den Trainings-Daten befüllen
-for img_number in range(0, 10000):
-    feature_space_x_data = (np.split(pixel_data_batch_1[img_number], 3))[0]
-    feature_space_y_data = (np.split(pixel_data_batch_1[img_number], 3))[1]
-    feature_space_z_data = (np.split(pixel_data_batch_1[img_number], 3))[2]
-    feature_space[feature_space_x_axis, feature_space_y_axis, feature_space_z_axis] = labels_data_batch_1[img_number] # Hier ist der feature space schon 4-dimensional
+for img_number in range(0, 9999):
+    pixel_data_current_img = np.asarray(pixel_data_batch_1_single_img[img_number])
+    print(pixel_data_current_img)
+    channel_data = np.split(pixel_data_current_img, 3)
+    feature_space_x_data = np.mean((channel_data)[0])
+    feature_space_y_data = np.mean((channel_data)[1])
+    feature_space_z_data = np.mean((channel_data)[2])
+    feature_space[feature_space_x_data, feature_space_y_data, feature_space_z_data] = labels_data_batch_1[img_number]   # Hier ist der feature space schon 4-dimensional
 
 
 
