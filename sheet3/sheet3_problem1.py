fc39c51fa49e8fb702ca3d076832d625964ab9c7
RealDebato <baraldennis99@googlemail.com>
2023-11-22 18:52:47 +0100

    Changes to be committed: 	modified:   sheet3/sheet3_problem1.py

--- sheet3/sheet3_problem1.py
+++ sheet3/sheet3_problem1.py
@@ -41,43 +41,56 @@ nr_bild = 0
 # main
 
 label_decoder = unpickle(R'sheet3\CIFAR\batches.meta.txt')             # Index = label nummer 
-print(label_decoder[b'label_names'][6])
+#print(label_decoder[b'label_names'][6])
 
 data_batch_1 = unpickle(R'sheet3\CIFAR\data_batch_1.bin')
 
-pixel_data_batch_1 = data_batch_1[b'data']
-labels_data_batch_1 = data_batch_1[b'labels']
+pixel_data_batch_1 = np.asarray(data_batch_1[b'data'])
+pixel_data_batch_1 = np.split(pixel_data_batch_1, 10000)    # hier steht jeweils ein Array mit allen Pixelwerten aus einem Bild (1. Array --> 1. Bild, 2. Array --> 2. Bild, usw.)
+labels_data_batch_1 = np.asarray(data_batch_1[b'labels'])
 
+
+print(pixel_data_batch_1.size)
 
 Bild1 = image_from_cifar10(pixel_data_batch_1[nr_bild,:])
 
-Entscheidungsraum_x = np.linspace(0, 3072, 1)
-Entscheidungsraum_y = np.linspace(0, 256, 1)
-Entscheidungsraum_z = np.linspace(0, 9999, 1)
-Entscheidungsraum = np.dstack(Entscheidungsraum_x, Entscheidungsraum_y, Entscheidungsraum_z)
-
-for Bild_nummer in range(0, 9999):
-    Entscheidungsraum[:,:,Bild_nummer] = [Entscheidungsraum_x, pixel_data_batch_1[Bild_nummer,:], Bild_nummer]
-
-
 
 # Wie sehen die Entscheidungsräume aus??
-# Features sollen die Pixel sein, d. h.
-# Entscheidungsraum x: Pixelnummer entsprechend aus dem .bin
-# Entscheidungsraum y: Pixelwert
-# Entscheidungsraum z: Bildnummer
+# Features sollen die Pixel sein, d. h. ich machs einfach und nehm den Mittelwert aller Pixel im gleichen Farbraum
+
+feature_space_x_axis = np.arange(0, 1023)   #red
+feature_space_y_axis = np.arange(0, 1023)   #green
+feature_space_z_axis = np.arange(0, 1023)   #blue
+
+feature_space = np.full((1024, 1024, 1024), fill_value=-1)
+
+X, Y, Z = np.meshgrid(feature_space_x_axis, feature_space_y_axis, feature_space_z_axis)
+
+
+
+# feature space mit den Trainings-Daten befüllen
+for img_number in range(0, 10000):
+    feature_space_x_data = (np.split(pixel_data_batch_1[img_number], 3))[0]
+    feature_space_y_data = (np.split(pixel_data_batch_1[img_number], 3))[1]
+    feature_space_z_data = (np.split(pixel_data_batch_1[img_number], 3))[2]
+    feature_space[feature_space_x_axis, feature_space_y_axis, feature_space_z_axis] = labels_data_batch_1[img_number] # Hier ist der feature space schon 4-dimensional
+
 
 
 #---------------------------------------------------------------------------------------------------
 # output
 
+feature_space_x_axis = np.arange(0, 1023)   #red
+feature_space_y_axis = np.arange(0, 1023)   #green
+feature_space_z_axis = np.arange(0, 1023)   #blue
+
 '''plt.figure
 plt.imshow(Bild1)
 plt.title(label_decoder[b'label_names'][labels_data_batch_1[nr_bild]])'''
 
-plt.figure
+'''plt.figure
 plt.imshow(Entscheidungsraum)
-plt.title('Entscheidungsraum')
+plt.title('Entscheidungsraum')'''
 
 
 #---------------------------------------------------------------------------------------------------
