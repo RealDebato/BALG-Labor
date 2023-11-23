51ec49959946c73a0b1363b241cfb83ed0a87c34
RealDebato <baraldennis99@googlemail.com>
2023-11-23 16:20:21 +0100

    Changes to be committed: 	modified:   sheet3/sheet3_problem1.py
    Differenzmessung im feature space funktioniert

--- sheet3/sheet3_problem1.py
+++ sheet3/sheet3_problem1.py
@@ -34,61 +34,148 @@ def image_from_cifar10(data_vektor):
 #---------------------------------------------------------------------------------------------------
 # globals
 
-nr_bild = 0
+nr_bild = 29
 
 
 #---------------------------------------------------------------------------------------------------
 # main
 
 label_decoder = unpickle(R'sheet3\CIFAR\batches.meta.txt')             # Index = label nummer 
-#print(label_decoder[b'label_names'][6])
+print(label_decoder[b'label_names'])
 
 data_batch_1 = unpickle(R'sheet3\CIFAR\data_batch_1.bin')
 
 pixel_data_batch_1 = np.asarray(data_batch_1[b'data'])
-pixel_data_batch_1_single_img = np.split(pixel_data_batch_1, 10000)    # hier steht jeweils ein Array mit allen Pixelwerten aus einem Bild (1. Array --> 1. Bild, 2. Array --> 2. Bild, usw.)
-#print(pixel_data_batch_1_single_img[0])
+
 labels_data_batch_1 = np.asarray(data_batch_1[b'labels'])
+
+# Einteilung in 4 Quartiele für 4-fold cross-validation
+split = 4
+split_col = pixel_data_batch_1.shape[0]/split
+split_col = int(split_col)
+
+# Testdaten
+pixel_data_batch_1_0_25 = pixel_data_batch_1[0:split_col, :]
+pixel_data_batch_1_25_50 = pixel_data_batch_1[split_col:2*split_col, :]
+pixel_data_batch_1_50_75 = pixel_data_batch_1[2*split_col:3*split_col, :]
+pixel_data_batch_1_75_100 = pixel_data_batch_1[3*split_col:4*split_col, :]
+
+# Trainingsdaten
+pixel_data_batch_1_0_75 = pixel_data_batch_1[0:3*split_col, :]
+pixel_data_batch_1_25_100 = pixel_data_batch_1[split_col:4*split_col, :]
+pixel_data_batch_1_without_25_50 = np.delete(pixel_data_batch_1, np.s_[split_col:2*split_col], 1)
+pixel_data_batch_1_without_50_75 = np.delete(pixel_data_batch_1, np.s_[split_col:2*split_col], 1)
 
 Bild1 = image_from_cifar10(pixel_data_batch_1[nr_bild,:])
 
 
 # Wie sehen die Entscheidungsräume aus??
-# Features sollen die Pixel sein, d. h. ich nehm den Mittelwert aller Pixel
+# Features sollen die Pixel sein, d. h. eine Dim ist ein Pixel [0:255]
+# Anzahl feature spaces entspricht Anzahl Labels = 0:9
+# Differenzen zu jedem feature space / label berechenen
 
-feature_space_x_axis = np.arange(0, 1023)   #red
-feature_space_y_axis = np.arange(0, 1023)   #green
-feature_space_z_axis = np.arange(0, 1023)   #blue
+# Zum Trainig werden nur 75% aller Bilder aus Batch 1 verwendet. Die restlichen 25% werden zum Testing verwendet 
 
-feature_space = np.full((256, 256, 256, 10), fill_value=10, dtype=np.uint16)
+# feature space 0 --> airplane
+class_0 = np.where(labels_data_batch_1==0)
+pixel_data_class_0 = np.asarray(pixel_data_batch_1[class_0, :][0])
+
+# feature space 1 --> automobile
+class_1 = np.where(labels_data_batch_1==1)
+pixel_data_class_1 = np.asarray(pixel_data_batch_1[class_1, :][0])
+
+# feature space 2 --> bird
+class_2 = np.where(labels_data_batch_1==2)
+pixel_data_class_2 = np.asarray(pixel_data_batch_1[class_2, :][0])
+
+# feature space 3 --> cat
+class_3 = np.where(labels_data_batch_1==3)
+pixel_data_class_3 = np.asarray(pixel_data_batch_1[class_3, :][0])
+
+# feature space 4 --> deer
+class_4 = np.where(labels_data_batch_1==4)
+pixel_data_class_4 = np.asarray(pixel_data_batch_1[class_4, :][0])
+
+# feature space 5 --> dog
+class_5 = np.where(labels_data_batch_1==5)
+pixel_data_class_5 = np.asarray(pixel_data_batch_1[class_5, :][0])
+
+# feature space 6 --> frog
+class_6 = np.where(labels_data_batch_1==6)
+pixel_data_class_6 = np.asarray(pixel_data_batch_1[class_6, :][0])
+
+# feature space 7 --> horse
+class_7 = np.where(labels_data_batch_1==7)
+pixel_data_class_7 = np.asarray(pixel_data_batch_1[class_7, :][0])
+
+# feature space 8 --> ship
+class_8 = np.where(labels_data_batch_1==8)
+pixel_data_class_8 = np.asarray(pixel_data_batch_1[class_8, :][0])
+
+# feature space 9 --> truck
+class_9 = np.where(labels_data_batch_1==9)
+pixel_data_class_9 = np.asarray(pixel_data_batch_1[class_9, :][0])
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+# Distanzberechnung für nur ein Testbild für jede Klasse einzeln
+# Delta zu Class 0
+#def delta_class(training, test):
 
 
-# feature space mit den Trainings-Daten befüllen
-for img_number in range(0, 9999):
-    pixel_data_current_img = np.asarray(pixel_data_batch_1_single_img[img_number])
-    print(pixel_data_current_img)
-    channel_data = np.split(pixel_data_current_img, 3)
-    feature_space_x_data = np.mean((channel_data)[0])
-    feature_space_y_data = np.mean((channel_data)[1])
-    feature_space_z_data = np.mean((channel_data)[2])
-    feature_space[feature_space_x_data, feature_space_y_data, feature_space_z_data] = labels_data_batch_1[img_number]   # Hier ist der feature space schon 4-dimensional
+
+k = 3
+
+delta = []
+delta_sum = []
+
+
+# Differenz von Test Img zu Class X-------------------------
+def distance_kNN(test, training_class, k):
+    training_class = np.asarray(training_class)
+    training_class = training_class.astype(np.double)
+    test = np.asarray(test)
+    test = test.astype(np.double)
+    delta = []
+    delta_sum = []
+    for number_trainings_img in range(0, training_class.shape[0]):
+        # new row for new image to compare 
+        diff = (test - training_class[number_trainings_img]) **2
+        delta = np.append(delta, diff)
+
+    delta = np.split(delta, training_class.shape[0])
+
+    for number_trainings_img in range(0, training_class.shape[0]):        # delta_sum[0] entspricht der Differenz zwischen dem Test bild und dem 1. Trainingsbild
+        delta_sum_single = delta[number_trainings_img].sum()
+        delta_sum = np.append(delta_sum, delta_sum_single)
+
+    sorted_delta_sum = np.sort(delta_sum, axis=None, kind='heapsort')
+    k_nearest_class_0 = sorted_delta_sum[0:k]
+    return k_nearest_class_0
+
+
+nearest_class_0 = distance_kNN(pixel_data_batch_1[0], pixel_data_class_9, 3)
+
+
+print(nearest_class_0)
+print(len(nearest_class_0))
+
+
+
+# abs. Diff vom jeweiligen (0-3071) Pixelwert zwischen jedem Bildvektor in pixel_data_class und dem Testbild
+# Diff zu der Klasse 0
+#for px_pos in range(0, 3072):
+
 
 
 
 #---------------------------------------------------------------------------------------------------
 # output
 
-feature_space_x_axis = np.arange(0, 1023)   #red
-feature_space_y_axis = np.arange(0, 1023)   #green
-feature_space_z_axis = np.arange(0, 1023)   #blue
 
 '''plt.figure
 plt.imshow(Bild1)
 plt.title(label_decoder[b'label_names'][labels_data_batch_1[nr_bild]])'''
 
-'''plt.figure
-plt.imshow(Entscheidungsraum)
-plt.title('Entscheidungsraum')'''
 
 
 #---------------------------------------------------------------------------------------------------
